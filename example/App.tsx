1274967e80995e52d18f9e84b5bf6efe64b46785
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2026-01-04 02:48:01 +0100

    Add examples

--- example/App.tsx
+++ example/App.tsx
@@ -1,73 +1,58 @@
-import { useEvent } from 'expo';
-import ExpoWatermark, { ExpoWatermarkView } from 'expo-watermark';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import WatermarkView from "expo-watermark";
+import { useState } from "react";
+import { Image, SafeAreaView, Switch, Text, View } from "react-native";
 
 export default function App() {
-  const onChangePayload = useEvent(ExpoWatermark, 'onChange');
-
+  const [preview, setPreview] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{ExpoWatermark.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{ExpoWatermark.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await ExpoWatermark.setValueAsync('Hello from JS!');
-            }}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: 24,
+        }}
+      >
+        <Text>Toggle preview:</Text>
+        <Switch value={preview} onValueChange={setPreview} />
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 12,
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={{ uri: "https://github.com/jakex7.png" }}
+          style={{ width: 50, height: 50, borderRadius: 25 }}
+        />
+        <View>
+          <Text style={{ fontWeight: "bold" }}>Jakub Grzywacz</Text>
+          <Text style={{ opacity: 0.6 }}>@k7grzywacz</Text>
+        </View>
+        <WatermarkView
+          backgroundColor={styles.container.backgroundColor}
+          preview={preview}
+          style={{ marginLeft: "auto" }}
+        >
+          <Image
+            source={require("./assets/logo.png")}
+            resizeMode="contain"
+            style={{ width: 30, height: 30 }}
           />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <ExpoWatermarkView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
-      </ScrollView>
+        </WatermarkView>
+      </View>
     </SafeAreaView>
   );
 }
 
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
-    </View>
-  );
-}
-
 const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-  },
-  view: {
-    flex: 1,
-    height: 200,
+    backgroundColor: "#fff",
+    margin: 24,
   },
 };
