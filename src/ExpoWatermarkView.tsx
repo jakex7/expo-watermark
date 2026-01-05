96c3b75f396c9cbcec58f0f48984105af4dcd153
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2026-01-05 15:40:43 +0100

    Revert "Remove adnroid"
    This reverts commit 87f8629a94a4caa9395def02a5399f69c18d1d2f.

--- src/ExpoWatermarkView.tsx
+++ src/ExpoWatermarkView.tsx
@@ -4,13 +4,10 @@ import * as React from "react";
 import { ExpoWatermarkViewProps } from "./ExpoWatermark.types";
 import { Platform, View } from "react-native";
 
-let NativeView: React.ComponentType<ExpoWatermarkViewProps>;
-
-if (Platform.OS === "ios") {
-  NativeView = requireNativeView("ExpoWatermark");
-}
+const NativeView: React.ComponentType<ExpoWatermarkViewProps> =
+  requireNativeView("ExpoWatermark");
 
 export default function ExpoWatermarkView(props: ExpoWatermarkViewProps) {
-  if (!NativeView) return <View {...props} />;
+  if (Platform.OS !== "ios") return <View {...props} />;
   return <NativeView {...props} />;
 }
